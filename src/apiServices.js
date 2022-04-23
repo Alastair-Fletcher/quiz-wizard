fb6f990a571be6b521f9b2096778df427f067ac9
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-04-23 12:00:45 +0200

    add server api services

---
+++ src/apiServices.js
@@ -0,0 +1,19 @@
+const baseURL = 'http://localhost:3001';
+
+//post questions
+export const postQuestion = async (question) => {
+  return fetch(`${baseURL}/questions`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(question),
+  })
+    .then((result) => result.json())
+    .catch((error) => console.log(error));
+};
+
+// get questions
+export const getQuestions = async () => {
+  return fetch(`${baseURL}/questions`)
+    .then((result) => result.json())
+    .catch((error) => console.log(error));
+};
