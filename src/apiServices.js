c39110615d75a72e16832468ed5d9b185b421aee
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-04-25 09:14:39 +0200

    add deleteQuestion function

--- src/apiServices.js
+++ src/apiServices.js
@@ -1,6 +1,6 @@
 const baseURL = 'http://localhost:3001';
 
-//post questions
+//============= post questions
 export const postQuestion = async (question) => {
   return fetch(`${baseURL}/questions`, {
     method: 'POST',
@@ -11,9 +11,21 @@ export const postQuestion = async (question) => {
     .catch((error) => console.log(error));
 };
 
-// get questions
+//============= get questions
 export const getQuestions = async () => {
   return fetch(`${baseURL}/questions`)
+    .then((result) => result.json())
+    .catch((error) => console.log(error));
+};
+
+// TODO -- "delete / edit questions"
+//============= delete questions
+export const deleteQuestion = async (question) => {
+  return fetch(`${baseURL}/questions`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(question),
+  })
     .then((result) => result.json())
     .catch((error) => console.log(error));
 };
