44cd6c2974e150d466ddef7a4b6a894142a47103
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-04-25 09:16:55 +0200

    add tailwind/daisyUI

--- src/components/CreateQuiz.js
+++ src/components/CreateQuiz.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState } from 'react';
 import { postQuestion } from '../apiServices';
 
@@ -9,7 +10,7 @@ function CreateQuiz() {
 
     const newQuestion = {
       category: question.target.category.value.toLowerCase(),
-      question: question.target.question.value.toLowerCase(),
+      question: question.target.question.value,
       a: {
         answer: question.target.a.value.toLowerCase(),
         correctAnswer: question.target.correctA.checked,
@@ -30,7 +31,7 @@ function CreateQuiz() {
 
     postQuestion(newQuestion)
       .then((dbQuestion) => {
-        setQuestions((previousQuestions) => [...previousQuestions, dbQuestion]);
+        setQuestions((prevQuestions) => [...prevQuestions, dbQuestion]);
         question.target.category.value = '';
         question.target.question.value = '';
         question.target.a.value = '';
@@ -43,107 +44,109 @@ function CreateQuiz() {
 
   return (
     <div className="create-quiz">
-      <form onSubmit={submitHandler}>
-        <label htmlFor="category">category</label>
-        <input
-          type="text"
-          name="category"
-          id="category"
-          placeholder="type category..."
-          required
-        />
-
-        <label htmlFor="question">question</label>
-        <input
-          type="text"
-          name="question"
-          id="question"
-          placeholder="type your q..."
-          required
-        />
-
-        <div className="options">
-          <div className="option">
-            <label htmlFor="a">a</label>
-            <input
-              type="text"
-              name="answer"
-              id="a"
-              placeholder="option a..."
-              required
-            />
-            <span className="radios">
-              <input
-                type="radio"
-                name="correctAnswer"
-                className="radio-md"
-                id="correctA"
-              />
-            </span>
-          </div>
-
-          <div className="option">
-            <label htmlFor="b">b</label>
-            <input
-              type="text"
-              name="answer"
-              id="b"
-              placeholder="option b..."
-              required
-            />
-            <span className="radios">
-              <input
-                type="radio"
-                name="correctAnswer"
-                className="radio-md"
-                id="correctB"
-              />
-            </span>
-          </div>
-
-          <div className="option">
-            <label htmlFor="c">c</label>
-            <input
-              type="text"
-              name="answer"
-              id="c"
-              placeholder="option c..."
-              required
-            />
-            <span className="radios">
-              <input
-                type="radio"
-                name="correctAnswer"
-                className="radio-md"
-                id="correctC"
-              />
-            </span>
-          </div>
-
-          <div className="option">
-            <label htmlFor="d">d</label>
-            <input
-              type="text"
-              name="answer"
-              id="d"
-              placeholder="option d..."
-              required
-            />
-
-            <span className="radios">
-              <input
-                type="radio"
-                name="correctAnswer"
-                className="radio-md"
-                id="correctD"
-              />
-            </span>
-          </div>
+      <form onSubmit={submitHandler} className="flex flex-col">
+        <div className="pt-8 px-8 pb-0 flex items-center justify-between">
+          <input
+            type="text"
+            name="category"
+            id="category"
+            className="input w-full input-primary"
+            placeholder="category..."
+            required
+          />
         </div>
+        <div className="pt-8 px-8 pb-0 flex items-center justify-between">
+          <input
+            type="text"
+            name="question"
+            id="question"
+            className="input w-full input-primary"
+            placeholder="question..."
+            required
+          />
+        </div>
+        <div className="pt-8 px-8 pb-0 flex items-center justify-between">
+          <input
+            type="text"
+            name="answer"
+            id="a"
+            className="input w-full"
+            placeholder="option a..."
+            required
+          />
+          <span className="radios pl-8">
+            <input
+              type="radio"
+              name="correctAnswer"
+              className="radio radio-md flex items-center"
+              id="correctA"
+            />
+          </span>
+        </div>
+        <div className="pt-8 px-8 pb-0 flex items-center justify-between">
+          <input
+            type="text"
+            name="answer"
+            id="b"
+            className="input w-full"
+            placeholder="option b..."
+            required
+          />
+          <span className="radios pl-8">
+            <input
+              type="radio"
+              name="correctAnswer"
+              className="radio radio-md flex items-center"
+              id="correctB"
+            />
+          </span>
+        </div>
+        <div className="pt-8 px-8 pb-0 flex items-center justify-between">
+          <input
+            type="text"
+            name="answer"
+            id="c"
+            className="input w-full"
+            placeholder="option c..."
+            required
+          />
+          <span className="radios pl-8">
+            <input
+              type="radio"
+              name="correctAnswer"
+              className="radio radio-md flex items-center"
+              id="correctC"
+            />
+          </span>
+        </div>
+        <div className="pt-8 px-8 pb-0 flex items-center justify-between">
+          <input
+            type="text"
+            name="answer"
+            id="d"
+            className="input w-full"
+            placeholder="option d..."
+            required
+          />
 
-        <button type="submit" id="submit" className="btn btn-secondary">
-          CREATE
-        </button>
+          <span className="radios pl-8">
+            <input
+              type="radio"
+              name="correctAnswer"
+              className="radio radio-md flex items-center"
+              id="correctD"
+            />
+          </span>
+        </div>
+        <div className="p-8">
+          <button
+            type="submit"
+            id="submit"
+            className="btn btn-secondary btn-block"
+          >
+            CREATE
+          </button>
+        </div>
       </form>
     </div>
   );
