beae2416cd62a5c1e9e3cffafc0f73085d0f99ab
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-05-21 20:12:54 +0200

    add fetchQuestions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.scss';
-
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
 import PlayQuiz from './components/PlayQuiz';
 import CreateQuiz from './components/CreateQuiz';
@@ -10,20 +8,30 @@ import { getQuestions } from './apiServices';
 
 function App() {
   const [questions, setQuestions] = useState([]);
+  const [fetchQuestions, setFetchQuestions] = useState(false);
 
   useEffect(() => {
     getQuestions().then((questions) => setQuestions(questions));
-  }, []);
+  }, [fetchQuestions]);
 
   return (
     <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<PlayQuiz questions={questions} />} />
-          <Route path="/create" element={<CreateQuiz />} />
-        </Routes>
-      </>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PlayQuiz
+              questions={questions}
+              setFetchQuestions={setFetchQuestions}
+            />
+          }
+        />
+        <Route
+          path="/create"
+          element={<CreateQuiz setFetchQuestions={setFetchQuestions} />}
+        />
+      </Routes>
     </Router>
   );
 }
