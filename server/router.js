cc50e4ac832bb15d56a7ca0e35be86464fec7797
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-05-21 19:54:42 +0200

    add deleteQuestion

--- server/router.js
+++ server/router.js
@@ -4,5 +4,6 @@ const QuestionsController = require('./controllers/controller');
 
 router.get('/questions', QuestionsController.getQuestions);
 router.post('/questions', QuestionsController.postQuestion);
+router.delete('/questions/:_id', QuestionsController.deleteQuestion);
 
 module.exports = router;
